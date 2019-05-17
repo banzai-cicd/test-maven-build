660a5efdea1b6ae73ac71e95572ee41114ff3602
Simon Townsend <simon.townsend1@ge.com>
2019-05-17 14:01:19 -0500

    gitops testing

--- publishScript.sh
+++ publishScript.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-echo "Running fake Publish" 
+echo "Running fake Publish"
+BUILD_VERSION=$(sed -n 's:.*<version>\(.*\)</version>.*:\1:p' pom.xml | sed 's|> |>\n|g;1q')
+echo "test-maven $BUILD_VERSION" > $WORKSPACE/gitOpsVersions
+echo "gitOpsVersions file created!"
