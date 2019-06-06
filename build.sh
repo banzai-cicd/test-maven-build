a9906286ee973f00208538a25eadd53b1e5462ca
Simon Townsend <simon.townsend1@ge.com>
2019-06-06 09:05:10 -0500

    BanzaiUserData

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 BUILDCMD="mvn -s ./settings.xml clean install -U"
 echo "BUILD SCRIPT - Building"
 eval $BUILDCMD
-echo "USER_DATA={'coverity' : { 'buildCmd' : '${BUILDCMD}' } }"
+echo "BanzaiUserData={'coverity' : { 'buildCmd' : '${BUILDCMD}' } }"
