a7352763949fd8bf49cd3c2a0d6d4c67c355522a
Simon Townsend <simon.townsend1@ge.com>
2019-06-06 10:02:24 -0500

    write userdata to file

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 BUILDCMD="mvn -s ./settings.xml clean install -U"
 echo "BUILD SCRIPT - Building"
 eval $BUILDCMD
-echo "BanzaiUserData={'coverity' : { 'buildCmd' : '${BUILDCMD}' } }"
+echo "{\"coverity\" : { \"buildCmd\" : \"${BUILDCMD}\" } }" > $WORKSPACE/BanzaiUserData.json
